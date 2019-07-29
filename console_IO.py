e61cd792c254ff1bfb0f542e2a7e27f34071067d
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-07-29 21:33:20 +0900

    4

--- console_IO.py
+++ console_IO.py
@@ -1,3 +1,6 @@
-name=input("이름이 무엇인가요? ")
-age=input("나이를 입력해주세요 : ")
-print("10년 후면",int(age)+10,"살이 되는군요")
+year = int(input("year을 입력하세요 :"))
+if(((year % 4 == 0)and(year % 100 != 0))or(year % 400 == 0)):
+    print(year, "년은 윤년입니다.")
+
+else:
+    print("%d년은 윤년이 아닙니다." % year)
