db55f9b16244e8efcd860e4d4aba3a1d24c2b698
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-07-30 17:36:20 +0900

    function2

--- function.py
+++ function.py
@@ -1,7 +1,17 @@
 def add(a, b):
-    result = a+b
-    return result
+    return a+b
 
 
-sum = add(100, 200)
-print(sum)
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
+
+a = int(input("정수 1 입력 : "))
+b = int(input("정수 2 입력 : "))
+sum = add(a, b)
+average = sum/2
+print("두 수의 합 : %d" % sum)
+print("두 수의 평균 : %d" % average)
+a, b = swap(a, b)
+print("두 수의 교환 : %d, %d" % (a, b))
