921da92b2f74e49182df0abfd98a6d90fe56287b
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-07-30 17:21:27 +0900

    while

--- while.py
+++ while.py
@@ -1,8 +1,10 @@
 i = 0
 hap = 0
 
-i = 1
-while i < 11:
+for i in range(1, 101):
     hap += i
-    i+=1
-print("1에서 10까지의 합 : ", hap)
+
+    if hap > 1000:
+        break
+
+print("1~100의 합에서 최초로 1000이 넘는 위치 : %d" % i)
