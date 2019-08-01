5c58a292a3b30392724151db2d124aed270a7260
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-08-01 14:53:09 +0900

    for

--- python_SOGANG/dictionary3.py
+++ python_SOGANG/dictionary3.py
@@ -1,7 +1,6 @@
 fruit={'배':[2,1000],'자몽':[1,2000],'메론':[1,8000],'감':[6,800]}
 fruit_want = input("먹고 싶은 과일은? : ")
 exist = fruit.get(fruit_want,0)
-print(exist)
 if exist==0:
     print("%s 준비되어 있지 않습니다"%fruit_want)
 else:
--- python_SOGANG/for.py
+++ python_SOGANG/for.py
@@ -3,4 +3,11 @@ for i in range(10):
     if number == 0:
         print("입력 받은 수가 0입니다.\n프로그램을 종료합니다.")
         break
-    if number > 0
+    if number > 0 and abs(number) % 2 == 0:
+        print("%d : 양수, 짝수" % number)
+    if number > 0 and abs(number) % 2 == 1:
+        print("%d : 양수, 홀수" % number)
+    if number < 0 and abs(number) % 2 == 0:
+        print("%d : 음수, 짝수" % number)
+    if number < 0 and number % 2 == 1:
+        print("%d : 음수, 홀수" % number)
